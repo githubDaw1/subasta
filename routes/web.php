4fb2853f92e02e597a7d39d3c0cf93eb1923eaf1
Rafael2026 <rafaaguilarmunoz@gmail.com>
2023-06-13 20:05:47 +0200

    Many changes

--- routes/web.php
+++ routes/web.php
@@ -1,71 +1,18 @@
 <?php
 
-  use Illuminate\Support\Facades\Route;
-  use App\Support\Csp\Policies\CustomPolicy;
-  use Spatie\Csp\AddCspHeaders;
+use Illuminate\Support\Facades\Route;
 
-  /*use App\Http\Controllers\PrincipalController;
-  use App\Http\Controllers\LoginController;
-  use App\Http\Controllers\RegistroController;
-  use App\Http\Controllers\PortalController;
-  use App\Http\Controllers\SubastaController;
-  use App\Http\Controllers\PujarController;
-  use App\Http\Controllers\TablasController;
-  use App\Http\Controllers\ProductosController;
-  use App\Http\Controllers\UsuariosController;*/
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-  /*
-  |--------------------------------------------------------------------------
-  | Web Routes
-  |--------------------------------------------------------------------------
-  |
-  | Here is where you can register web routes for your application. These
-  | routes are loaded by the RouteServiceProvider within a group which
-  | contains the "web" middleware group. Now create something great!
-  |
-  */
-
-  //Route::post('/', [PrincipalController::class, 'request']);
-  //Route::post('/login', [LoginController::class, 'request']);
-  //Route::post('/registro', [RegistroController::class, 'request']);
-  //Route::post('/portal', [PortalController::class, 'request']);
-  //Route::post('/subasta', [SubastaController::class, 'request']);
-  //Route::post('/pujar', [PujarController::class, 'request']);
-  //Route::post('/tablas', [TablasController::class, 'request']);
-  //Route::post('/producto', [ProductosController::class, 'request']);
-
-  Route::get('/', function() {
-    return view('principal');
-  });
-
-  /*Route::get('/login', function() {
-    return view('login');
-  });
-
-  Route::get('/registro', function() {
-    return view('registro');
-  });*/
-
-  Route::get('/portal', function() {
-    return view('portal');
-  });
-
-  Route::get('/subasta', function() {
-    return view('subasta');
-  });
-
-  Route::get('/pujas', function() {
-    return view('pujas');
-  });
-
-  Route::get('/tablas', function() {
-    return view('tablas');
-  });
-
-  /*Route::get('/producto', function() {
-    return view('producto');
-  });*/
-
-  //Route::get('example-route', 'ExampleController')->middleware(AddCspHeaders::class);
-
-?>
+Route::get('/', function () {
+    return view('welcome');
+});
